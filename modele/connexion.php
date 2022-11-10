bcb07f90c7a15d62af238c626743d86b7141ac0e
VELLA Matteo <mattteo.vella1@etu.univ-lorraine.fr>
2022-11-10 08:18:31 +0100

    création de la branche modif

--- modele/connexion.php
+++ modele/connexion.php
@@ -2,42 +2,42 @@
 
 // Connexion avec le serveur de l'IUT
 
-// $db_config['SGBD'] = 'mysql';
-// $db_config['HOST'] = 'devbdd.iutmetz.univ-lorraine.fr';
-// $db_config['DB_NAME'] = 'dellaera2u_tpwebdevweb';
-// $db_config['USER'] = 'dellaera2u_appli';
-// $db_config['PASSWORD'] = '32002011';
+$db_config['SGBD'] = 'mysql';
+$db_config['HOST'] = 'devbdd.iutmetz.univ-lorraine.fr';
+$db_config['DB_NAME'] = 'dellaera2u_tpwebdevweb';
+$db_config['USER'] = 'dellaera2u_appli';
+$db_config['PASSWORD'] = '32002011';
 
-// try {
-//     $db = new PDO(
-//         $db_config['SGBD'] . ':host=' . $db_config['HOST'] . ';dbname=' . $db_config['DB_NAME'],
-//         $db_config['USER'],
-//         $db_config['PASSWORD'],
-//         array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
-//     );
-//     unset($db_config);
-// } catch (Exception $exception) {
-//     die($exception->getMessage());
-// }
+try {
+    $db = new PDO(
+        $db_config['SGBD'] . ':host=' . $db_config['HOST'] . ';dbname=' . $db_config['DB_NAME'],
+        $db_config['USER'],
+        $db_config['PASSWORD'],
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
+    );
+    unset($db_config);
+} catch (Exception $exception) {
+    die($exception->getMessage());
+}
 
 
 
 
 // Connexion avec le serveur WAMP
 
-$db_config['SGBD'] = 'mysql';
-$db_config['HOST'] = 'localhost';
-$db_config['DB_NAME'] = 'tpweb_devweb';
-$db_config['USER'] = 'root';
-$db_config['PASSWORD'] = '';
+// $db_config['SGBD'] = 'mysql';
+// $db_config['HOST'] = 'localhost';
+// $db_config['DB_NAME'] = 'tpweb_devweb';
+// $db_config['USER'] = 'root';
+// $db_config['PASSWORD'] = '';
 
-try {
-    $db = new PDO($db_config['SGBD'].':host='.$db_config['HOST'].';dbname='.$db_config['DB_NAME'], $db_config['USER'], $db_config['PASSWORD'], 
-    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+// try {
+//     $db = new PDO($db_config['SGBD'].':host='.$db_config['HOST'].';dbname='.$db_config['DB_NAME'], $db_config['USER'], $db_config['PASSWORD'], 
+//     array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-    unset($db_config);
-} catch (Exception $exception) {
-    die($exception->getMessage());
-}
+//     unset($db_config);
+// } catch (Exception $exception) {
+//     die($exception->getMessage());
+// }
 
 ?>
